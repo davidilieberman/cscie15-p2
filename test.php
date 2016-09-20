45f70ebf93a5068f732656a671bfbeaf5a31000c
David Lieberman <dlieberman@hbs.edu>
2016-09-20 18:40:17 -0400

    Made block to output current state of words array a function; fixed bug where random number function was returning values that exceed the length of the words array.

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+error_reporting(E_ALL);       // Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); // Display errors on page (instead of a log file)
 ?>
 
 <!DOCTYPE html>
@@ -9,28 +9,35 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
     <title>Test</title>
     <?php
 
-      $lines = file("./words.txt");
+      $lines = file('./words.txt');
+
+      function echoLines($lines)
+      {
+          echo "<p>\n";
+          foreach ($lines as $line_num => $line) {
+              $l = trim($line);
+              echo "Line #<b>{$line_num}</b>: \"${l}\" <br/>\n";
+          }
+          echo "</p>\n";
+      }
 
      ?>
   </head>
 
   <?php
 
-    foreach ($lines as $line_num => $line) {
-      $l = trim($line);
-      echo "Line #<b>{$line_num}</b>: \"${l}\" <br/>\n";
-    }
+    echoLines($lines);
 
-    $pwd = "";
-    for ($i = 0; $i < 4; $i++) {
-      $r = mt_rand(0, count($lines));
-      $pwd .= trim(array_splice($lines, $r, 1)[0]);
-      if ($i < 3) {
-        $pwd .= "-";
-      }
+    $pwd = '';
+    for ($i = 0; $i < 4; ++$i) {
+        $r = mt_rand(0, count($lines)-1);
+        $pwd .= trim(array_splice($lines, $r, 1)[0]);
+        if ($i < 3) {
+            $pwd .= '-';
+        }
+        echoLines($lines);
+        echo "<p>Password is $pwd</p>\n";
     }
-
-    echo "<p>Password is $pwd</p>\n";
 
    ?>
 </html>
